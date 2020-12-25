04b2b9efd933ec0f284e304ae3a9cf05c0870d0d
sanjeeev <sanjeevsingh.ss342@gmail.com>
2020-12-25 10:45:23 +0530

    inital commit tut1 is added

---
+++ tut2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+// This program was created by sanjeev -->single line comments
+/* this
+is 
+a 
+multi
+line 
+comment */
+int main(){
+    int sum = 6;
+    cout<< "Hello world"<< sum;
+    return 0;
+}
